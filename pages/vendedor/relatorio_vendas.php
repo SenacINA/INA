e0695b7f885b8a52afe9b104140dbc58e4151bd2
Hawstyyy <hawstyyy@proton.me>
2024-12-10 10:56:24 -0400

    pagina de relatorio de vendas 35% pronta

---
+++ pages/vendedor/relatorio_vendas.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E ao Quadrado</title>
+    <link rel="stylesheet" href="../../css/vendedor/relatorio_vendas.css">
+    <link rel="shortcut icon" href="../../image/geral/icone_eaoquadrado.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+  <nav></nav>
+  <img src="../../image/vendedor/relatorio_vendas/header.png" alt="">
+  <main class="main_content">
+    <div class="quadrado">
+      <div class="pesquisar_pedidos"></div>
+      <div class="estatisticas"></div>
+    </div>
+    <div class="filtro"></div>
+    <div class="table"></div>
+  </main>
+</body>
+</html>
