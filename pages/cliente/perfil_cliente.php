e626c01351e058fd5a79b9bb77beb7541fc8a380
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2025-01-27 09:57:19 -0400

    Rename das Class da pag. 'Perfil Cliente' - Cliente

--- pages/cliente/perfil_cliente.php
+++ pages/cliente/perfil_cliente.php
@@ -13,16 +13,16 @@
 
 </head>
 <body>
-    <nav class="nav_bar"></nav>
+    <nav class="perfil_cliente_nav_bar"></nav>
     <main>
-        <img src="../../image/cliente/perfil_cliente/banner_user.png" alt="banner" class="banner_cliente">
+        <img src="../../image/cliente/perfil_cliente/banner_user.png" alt="banner" class="perfil_cliente_banner_cliente">
 
-        <div class="pfp_cliente">
+        <div class="perfil_cliente_pfp_cliente">
             <img src="../../image/cliente/perfil_cliente/foto_user.png" alt="pfp_cliente" >
             <h1>Cliente 10</h1>
         </div>
 
-        <div class="botao_menu">
+        <div class="perfil_cliente_botao_menu">
             <form action="">
                 <select name="menu" id="menu"> 
                     <option value="">Menu</option>
@@ -33,17 +33,17 @@
             </form>  
         </div>
 
-        <div class="grid_principal">
-            <div class="infos_container">
-                <div class="infos_item1">
+        <div class="perfil_cliente_grid_principal">
+            <div class="perfil_cliente_infos_container">
+                <div class="perfil_cliente_infos_item1">
                     <img src="../../image/cliente/perfil_cliente/icon_localizacao_cliente.svg">
                     <p>Produtos: 8</p>
                 </div>
-                <div class="infos_item2">
+                <div class="perfil_cliente_infos_item2">
                     <img src="../../image/cliente/perfil_cliente/chat_user.png">
                     <p>Avaliação geral: 4.5</p>
                 </div>
-                <div class="infos_item3">
+                <div class="perfil_cliente_infos_item3">
                     <img src="../../image/cliente/perfil_cliente/membro_user.png">
                     <p>cliente há: 4 Meses</p>
                 </div>
@@ -51,84 +51,84 @@
 
             <hr>
 
-            <div class="about_container">
+            <div class="perfil_cliente_about_container">
                 <img src="../../image/cliente/perfil_cliente/icon_contato_user.png">
                 <h1>Contatos</h1>
             </div>
             
-            <div class="contatos_cliente">
-                <img src="../../image/cliente/perfil_cliente/icon_instagram_cliente.svg" class="icon_instagram_cliente">
-                <a href="#" class="instagram_cliente">my.cliente10</a>
+            <div class="perfil_cliente_contatos_cliente">
+                <img src="../../image/cliente/perfil_cliente/icon_instagram_cliente.svg" class="perfil_cliente_icon_instagram_cliente">
+                <a href="#" class="perfil_cliente_instagram_cliente">my.cliente10</a>
             
-                <img src="../../image/cliente/perfil_cliente/icon_facebook_cliente.svg" class="icon_facebook_cliente">
-                <a href="#" class="facebook_cliente">cliente10</a>
+                <img src="../../image/cliente/perfil_cliente/icon_facebook_cliente.svg" class="perfil_cliente_icon_facebook_cliente">
+                <a href="#" class="perfil_cliente_facebook_cliente">cliente10</a>
             </div>  
 
             <hr>
 
-            <div class="grid_historico">
-                <div class="about_container_2">
+            <div class="perfil_cliente_grid_historico">
+                <div class="perfil_cliente_about_container_2">
                     <img src="../../image/cliente/perfil_cliente/icon_lojinha.png" alt="Icon Loja">
                     <h1>Histórico:</h1>
                 </div>  
             
-                <div class="grid_item">
-                    <div class="frame_img">
+                <div class="perfil_cliente_grid_item">
+                    <div class="perfil_cliente_frame_img">
                         <img src="../../image/cliente/perfil_cliente/teclado_gamer.png" alt="Teclado Gamer">
                     </div>
             
-                    <div class="produto_item">
+                    <div class="perfil_cliente_produto_item">
                         <div>
                             <h1>Teclado mecânico Redragon Kumara</h1>
                             <h2>★★★★★</h2>
                         </div>
                         
-                        <div class="produto_status">
+                        <div class="perfil_cliente_produto_status">
                             <h2>Avaliado</h2>
                             <h4>Pedido Entregue</h4>
                             <h3>x1</h3>
                         </div>
-                        <div class="produto_preco">
+                        <div class="perfil_cliente_produto_preco">
                             <h3>R$235,18</h3>
                             <h4>R$147,99</h4>
                         </div>
-                        <div class="produto_total">
+                        <div class="perfil_cliente_produto_total">
                             <h1>Total do pedido: R$1421,00</h1>
                         </div>
                     </div>
             
-                    <div class="grid_botoes">
-                        <div class="btn_1"><h1>Compra Novamente</h1></div>
+                    <div class="perfil_cliente_grid_botoes">
+                        <div class="perfil_cliente_btn_1"><h1>Compra Novamente</h1></div>
                     </div>
                 </div>
-                <hr class="hr_2">
-                <div class="grid_item">
-                    <div class="frame_img">
+                <hr class="perfil_cliente_hr_2">
+                <div class="perfil_cliente_grid_item">
+                    <div class="perfil_cliente_frame_img">
                         <img src="../../image/cliente/perfil_cliente/teclado_gamer.png" alt="Teclado Gamer">
                     </div>
             
-                    <div class="produto_item">
+                    <div class="perfil_cliente_produto_item">
                         <div>
                             <h1>Teclado mecânico Redragon Kumara</h1>
                             <h2>★★★★★</h2>
                         </div>
                         
-                        <div class="produto_status">
+                        <div class="perfil_cliente_produto_status">
                             <h2>Avaliado</h2>
                             <h4>Pedido Entregue</h4>
                             <h3>x1</h3>
                         </div>
-                        <div class="produto_preco">
+                        <div class="perfil_cliente_produto_preco">
                             <h3>R$235,18</h3>
                             <h4>R$147,99</h4>
                         </div>
-                        <div class="produto_total">
+                        <div class="perfil_cliente_produto_total">
                             <h1>Total do pedido: R$1421,00</h1>
                         </div>
                     </div>
             
-                    <div class="grid_botoes">
-                        <div class="btn_1"><h1>Compra Novamente</h1></div>
+                    <div class="perfil_cliente_grid_botoes">
+                        <div class="perfil_cliente_btn_1"><h1>Compra Novamente</h1></div>
                     </div>
                 </div>
             </div>
@@ -137,7 +137,7 @@
     </main>
      
     <footer>
-      <img class="footer_img" src="../../image/cliente/footer/img_footer_placeholder.png">
+      <img class="perfil_cliente_footer_img" src="../../image/cliente/footer/img_footer_placeholder.png">
     </footer>
 </body>
 </html>
