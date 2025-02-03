5c8a137886012627fc04f4dac0a1828f7b979916
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2025-02-03 11:01:51 -0400

    fazendo pag 'Confirmar Pedido - Vendedor'

--- pages/vendedor/confirmar_pedido.php
+++ pages/vendedor/confirmar_pedido.php
@@ -21,25 +21,130 @@
                 <h2>05 de julho 2024, 17:37</h2>
             </div>
             <div class="botoes_status">
-                    <div class="botao_1">
-                        <h2>Pagamento Pendente </h2>
-                    </div>
-                    <div class="botao_2">
-                        <h2>Não processado</h2>
-                    </div>
+                <div class="botao_1">
+                    <h2>O Pagamento Pendente</h2>
+                </div>
+                <div class="botao_2">
+                    <h2>O Não processado</h2>
+                </div>
             </div>
         </div>
         
         <div class="grid_principal">
             <div class="container_1">
-                a
+                <div class="itens">
+                    <div class="produtos">
+                        <img src="../../image/vendedor/confirmar_pedido/mouse.png" alt="">
+                        <h1>Mouse Logitech G203</h1>
+                        <h2>R$170,91 x 1</h2>
+                        <h2>R$170,91</h2>
+                    </div>
+                    <div class="produtos">
+                        <img src="../../image/vendedor/confirmar_pedido/teclado.png" alt="">
+                        <h1>Thermaltake Pacific, R1 Plus Ddr4</h1>
+                        <h2>R$542,33 x 2</h2>
+                        <h2>R$1.084,66</h2>
+                    </div>
+                    <div class="produtos">
+                        <img src="../../image/vendedor/confirmar_pedido/teclado.png" alt="">
+                        <h1>Thermaltake Pacific, R1 Plus Ddr4</h1>
+                        <h2>R$542,33 x 2</h2>
+                        <h2>R$1.084,66</h2>
+                    </div>
+                    <div class="produtos">
+                        <img src="../../image/vendedor/confirmar_pedido/teclado.png" alt="">
+                        <h1>Thermaltake Pacific, R1 Plus Ddr4</h1>
+                        <h2>R$542,33 x 2</h2>
+                        <h2>R$1.084,66</h2>
+                    </div>  
+                    <div class="produtos">
+                        <img src="../../image/vendedor/confirmar_pedido/teclado.png" alt="">
+                        <h1>Thermaltake Pacific, R1 Plus Ddr4</h1>
+                        <h2>R$542,33 x 2</h2>
+                        <h2>R$1.084,66</h2>
+                    </div>  
+                </div>
+                
+                <div class="botao_confirmar_envio">
+                    <h1>Confirmar Envio</h1>
+                </div>
+
             </div>
+
             <div class="container_2">
-                a
+                <h1>Observações</h1>
+                <img src="../../image/vendedor/confirmar_pedido/lapis.png" alt="">
+                <div class="observacao">
+                    <textarea>Não há observações do cliente</textarea>
+                </div>
+                
             </div>
+
+
             <div class="container_3">
-                a
+                <div class="titulo_cronograma">
+                    <img src="../../image/vendedor/confirmar_pedido/relogio_preto.png" alt="">
+                    <h1>Cronograma</h1>
+                    <hr>
+                </div>
+                    
+                <div class="itens_container_3">
+                    <div class="container_3_item">
+                        <div>
+                            <hr>
+                            <img src="../../image/vendedor/confirmar_pedido/bolinha.png" alt="">
+                        </div>
+                        <div>
+                            <h3>Um pagamento de</h3>  
+                            <h2>R$1.469,01</h2>
+                            <h3>está pendente.</h3>
+                        </div>
+                        
+                    </div>
+
+                    <div class="container_3_item">
+                        <div>
+                            <img src="../../image/vendedor/confirmar_pedido/bolinha.png" alt="">
+                        </div>
+                        <div>
+                            <h1>Guilherme Xavier</h1> 
+                            <h3>fez o pedido em</h3>
+                            <h2>05/07/2024</h2> 
+                            <h3>às</h3>
+                            <h2>17:37.</h2>
+                        </div>
+                        
+                    </div>
+                    <div class="container_3_item">
+                        <div>
+                            <img src="../../image/vendedor/confirmar_pedido/bolinha.png" alt="">
+                        </div>
+                        <div>
+                            <h1>João Silva</h1> 
+                            <h3>realizou a compra em</h3>
+                            <h2>10/10/2024</h2> 
+                            <h3>às</h3>
+                            <h2>14:20.</h2>
+                        </div>
+                    </div>
+
+                    <div class="container_3_item">
+                        <div>
+                            <img src="../../image/vendedor/confirmar_pedido/bolinha.png" alt="">
+                        </div>
+                        <div>
+                            <h1>Ana Oliveira</h1> 
+                            <h3>fez o pedido em</h3>
+                            <h2>12/12/2024</h2> 
+                            <h3>às</h3>
+                            <h2>18:45.</h2>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
+
             <div class="container_4">
                 a
             </div>
