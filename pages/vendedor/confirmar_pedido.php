2545ab7fc89d9f542e0b4b12a8cbedc30fd8d6d0
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2025-02-05 18:01:30 -0400

    Conclusão da Task: Fazer estrutura da tela 'Confirmar Pedido' - Vendedor #243

--- pages/vendedor/confirmar_pedido.php
+++ pages/vendedor/confirmar_pedido.php
@@ -11,53 +11,53 @@
     <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <nav class="nav_bar"></nav>
-    <div class="grid_bg">
-        <div class="titulo"><h1>CONFIRMAR PEDIDO</h1></div>
+    <nav class="confirmar_pedido_nav_bar"></nav>
+    <div class="confirmar_pedido_grid_bg">
+        <div class="confirmar_pedido_titulo"><h1>CONFIRMAR PEDIDO</h1></div>
         <img src="../../image/geral/linha-divisoria-azul.png" alt="">
-        <div class="grid_id_pedido">
-            <div class="id_pedido">
+        <div class="confirmar_pedido_grid_id_pedido">
+            <div class="confirmar_pedido_id_pedido">
                 <h1>#1001</h1>
                 <h2>05 de julho 2024, 17:37</h2>
             </div>
-            <div class="botoes_status">
-                <div class="botao_1">
+            <div class="confirmar_pedido_botoes_status">
+                <div class="confirmar_pedido_botao_1">
                     <h2>O Pagamento Pendente</h2>
                 </div>
-                <div class="botao_2">
+                <div class="confirmar_pedido_botao_2">
                     <h2>O Não processado</h2>
                 </div>
             </div>
         </div>
         
-        <div class="grid_principal">
-            <div class="container_1">
-                <div class="itens">
-                    <div class="produtos">
+        <div class="confirmar_pedido_grid_principal">
+            <div class="confirmar_pedido_container_1">
+                <div class="confirmar_pedido_itens">
+                    <div class="confirmar_pedido_produtos">
                         <img src="../../image/vendedor/confirmar_pedido/mouse.png" alt="">
                         <h1>Mouse Logitech G203</h1>
                         <h2>R$170,91 x 1</h2>
                         <h2>R$170,91</h2>
                     </div>
-                    <div class="produtos">
+                    <div class="confirmar_pedido_produtos">
                         <img src="../../image/vendedor/confirmar_pedido/teclado.png" alt="">
                         <h1>Thermaltake Pacific, R1 Plus Ddr4</h1>
                         <h2>R$542,33 x 2</h2>
                         <h2>R$1.084,66</h2>
                     </div>
-                    <div class="produtos">
+                    <div class="confirmar_pedido_produtos">
                         <img src="../../image/vendedor/confirmar_pedido/teclado.png" alt="">
                         <h1>Thermaltake Pacific, R1 Plus Ddr4</h1>
                         <h2>R$542,33 x 2</h2>
                         <h2>R$1.084,66</h2>
                     </div>
-                    <div class="produtos">
+                    <div class="confirmar_pedido_produtos">
                         <img src="../../image/vendedor/confirmar_pedido/teclado.png" alt="">
                         <h1>Thermaltake Pacific, R1 Plus Ddr4</h1>
                         <h2>R$542,33 x 2</h2>
                         <h2>R$1.084,66</h2>
                     </div>  
-                    <div class="produtos">
+                    <div class="confirmar_pedido_produtos">
                         <img src="../../image/vendedor/confirmar_pedido/teclado.png" alt="">
                         <h1>Thermaltake Pacific, R1 Plus Ddr4</h1>
                         <h2>R$542,33 x 2</h2>
@@ -65,32 +65,32 @@
                     </div>  
                 </div>
                 
-                <div class="botao_confirmar_envio">
+                <div class="confirmar_pedido_botao_confirmar_envio">
                     <img src="../../image/vendedor/confirmar_pedido/botao_confirmar.png" alt="">
                     <h1>Confirmar Envio</h1>
                 </div>
 
             </div>
 
-            <div class="container_2">
+            <div class="confirmar_pedido_container_2">
                 <h1>Observações</h1>
                 <img src="../../image/vendedor/confirmar_pedido/lapis.png" alt="">
-                <div class="observacao">
+                <div class="confirmar_pedido_observacao">
                     <textarea>Não há observações do cliente</textarea>
                 </div>
                 
             </div>
 
 
-            <div class="container_3">
-                <div class="titulo_cronograma">
+            <div class="confirmar_pedido_container_3">
+                <div class="confirmar_pedido_titulo_cronograma">
                     <img src="../../image/vendedor/confirmar_pedido/relogio_preto.png" alt="">
                     <h1>Cronograma</h1>
                     <hr>
                 </div>
                     
-                <div class="itens_container_3">
-                    <div class="container_3_item">
+                <div class="confirmar_pedido_itens_container_3">
+                    <div class="confirmar_pedido_container_3_item">
                         <div>
                             <hr>
                             <img src="../../image/vendedor/confirmar_pedido/bolinha.png" alt="">
@@ -100,10 +100,9 @@
                             <h2>R$1.469,01</h2>
                             <h3>está pendente.</h3>
                         </div>
-                        
                     </div>
 
-                    <div class="container_3_item">
+                    <div class="confirmar_pedido_container_3_item">
                         <div>
                             <img src="../../image/vendedor/confirmar_pedido/bolinha.png" alt="">
                         </div>
@@ -116,7 +115,7 @@
                         </div>
                         
                     </div>
-                    <div class="container_3_item">
+                    <div class="confirmar_pedido_container_3_item">
                         <div>
                             <img src="../../image/vendedor/confirmar_pedido/bolinha.png" alt="">
                         </div>
@@ -129,7 +128,7 @@
                         </div>
                     </div>
 
-                    <div class="container_3_item">
+                    <div class="confirmar_pedido_container_3_item">
                         <div>
                             <img src="../../image/vendedor/confirmar_pedido/bolinha.png" alt="">
                         </div>
@@ -144,15 +143,15 @@
                 </div>
             </div>
 
-            <div class="container_4">
-                <div class=itens_container_4>
-                    <div class="container_4_item">
+            <div class="confirmar_pedido_container_4">
+                <div class="confirmar_pedido_itens_container_4">
+                    <div class="confirmar_pedido_container_4_item">
                         <h2>Subtotal</h2>
                         <h2>3 itens</h2>
                         <h2>R$1.2225,57</h2>
                     </div>
 
-                    <div class="container_4_item">
+                    <div class="confirmar_pedido_container_4_item">
                         <h2>Tributos</h2>
                         <h2>VAT 17%</h2>
                         <h2>R$213,44</h2>
@@ -161,45 +160,45 @@
                     <h1>Total</h1>
                     <hr>
                     
-                    <div class="container_4_total">
+                    <div class="confirmar_pedido_container_4_total">
                         <h2>Pago pelo Cliente</h2>
                         <h2>R$0</h2>
                     </div>
                 </div>
 
-                <div class="botao_receber_pagamento">
+                <div class="confirmar_pedido_botao_receber_pagamento">
                     <img src="../../image/vendedor/confirmar_pedido/seta_cima.png" alt="">
                     <h1>Receber Pagamento</h1>
                 </div>
             </div>
             
-            <div class="container_5">
-                <div class="container_5_item">
+            <div class="confirmar_pedido_container_5">
+                <div class="confirmar_pedido_container_5_item">
                     <h1>Cliente</h1>
                     <h2>Guilherme Xavier</h2>
                     <h2>3 pedidos</h2>
                 </div>
 
-                <div class="container_5_item">
-                    <div class="linha">
+                <div class="confirmar_pedido_container_5_item">
+                    <div class="confirmar_pedido_linha">
                         <h1>Informações de Contado</h1>
                         <img src="../../image/vendedor/confirmar_pedido/copiar.png" alt="">
                     </div>
                     <h2>(67) 9 9999-9999</h2>
                 </div>
 
-                <div class="container_5_item">
-                    <div class="linha">
+                <div class="confirmar_pedido_container_5_item">
+                    <div class="confirmar_pedido_linha">
                         <h1>Endereço de Entrega</h1>
                         <img src="../../image/vendedor/confirmar_pedido/copiar.png" alt="">
                     </div>
-                    <div class="linha_2">
+                    <div class="confirmar_pedido_linha_2">
                         <h3>CEP:</h3>
                         <h2>69910-890</h2>
                     </div>
                     <h2>Rua Vinte e Três, Tancredo Neves - Rio Branco, AC</h2>
                 </div>
-                <div class="botao_denunciar_usuario">
+                <div class="confirmar_pedido_botao_denunciar_usuario">
                     <img src="../../image/vendedor/confirmar_pedido/cancelar.png" alt="">
                     <h1>Denunciar Usuário</h1>
                 </div>
