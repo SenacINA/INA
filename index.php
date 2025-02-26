ddfe8190458bceb1760ab11ffe8e1bea5cf89560
eliasasa <eliasnetoneto1@hotmail.com>
2025-02-25 20:53:49 -0400

    index(home) 70%

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../../image/geral/icone_eaoquadrado.ico">
     <link rel="stylesheet" href="./css/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"> 
     <title>E ao Quadrado</title>
 </head>
 <body>
@@ -35,8 +36,70 @@
     </div>
 </header>
 <div class='container_main'>
-    <div class='bg_index_img'>
-        <img src="./image/index/fundoCarrossel.jpg" alt="">
+    <div class='carrossel'>
+        <div class='carrossel_content'>
+            <button class='carrossel_but back'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19"/>
+                </svg>
+            </button>
+            <div class='text_content'>
+                <h2>CONSOLES</h2>
+                <h3>Experimente a próxima geração de entretenimento com nossos consoles de última tecnologia!</h3>
+            </div>
+            <img id='fg_image' src="./image/index/fgCarrosselEx.png" alt="">
+            <button class='carrossel_but forward'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19"/>
+                </svg> 
+            </button>
+        </div>
+        <div class='carrossel_nav'>
+            <button class='active'></button>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+        <div class='categorias_nav'>
+            <div class='block_categorias'>
+                <img src="./image/index/carrosselHardware.png" alt="">
+                <p>Hardware</p>
+            </div>
+            <div class='block_categorias'>
+                <img src="./image/index/carrosselPerifericos.png" alt="">
+                <p>Periféricos</p>
+            </div>
+            <div class='block_categorias'>
+                <img src="./image/index/carrosselEscritorio.png" alt="">
+                <p>Escritório</p>
+            </div>
+            <div class='block_categorias'>
+                <img src="./image/index/carrosselCelulares.png" alt="">
+                <p>Celulares</p>
+            </div>
+            <div class='block_categorias'>
+                <img src="./image/index/carrosselEletro.png" alt="">
+                <p>Eletrodomésticos</p>
+            </div>
+        </div>
+    </div>
+    <div class='bg_container'>
+        <div class='content_container'>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+            <h1>oi</h1>
+        </div>
     </div>
 </div>
 
