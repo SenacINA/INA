b93cc0c252670179475f14e2911c804a244b3947
Hawstyyy <hawstyyy@proton.me>
2025-02-27 09:02:44 -0400

    fazendo a ligação da parte do vendedor

---
+++ js/vendedor/perfil_vendedor.js
@@ -0,0 +1,27 @@
+const menu = document.getElementById("menu");
+
+menu.addEventListener("change", () => {
+  switch (menu.value) {
+    case "editar-perfil":
+      menu.selectedIndex = 0;
+      window.location.href = "../../pages/vendedor/editar_perfil_vendedor.php";
+
+    case "pedidos":
+      menu.selectedIndex = 0;
+    // tela faltante
+    
+    case "relatorio":
+      menu.selectedIndex = 0;
+      window.location.href = "../../pages/vendedor/relatorio_vendas.php";
+
+    case "criar-cupom":
+      menu.selectedIndex = 0;
+    // tela faltante
+
+    case "editar-produtos":
+      menu.selectedIndex = 0;
+    // tela faltante
+
+    case "sair":
+  }
+});
