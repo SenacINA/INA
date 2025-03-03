e58a8cc14c76f71d0f901302f5aa422c7ae3b9cf
Davi <samuraiblack08@gmail.com>
2025-03-03 14:57:07 -0400

    Commit Add

--- pages/vendedor/cadastro_vendedor_1.php
+++ pages/vendedor/cadastro_vendedor_1.php
@@ -11,11 +11,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="nav_bar"></div>
-    <div class="grid_bg" >
-        <div class="titulo"><h1>CADASTRO DE VENDEDOR</h1></div>
-        <div class="cadastro_row"></div>
-        <div class="lista_ordenada" >
+    <div class="cadastro_vendedor1_nav_bar"></div>
+    <div class="cadastro_vendedor1_grid_bg" >
+        <div class="cadastro_vendedor1_titulo"><h1>CADASTRO DE VENDEDOR</h1></div>
+        <div class="cadastro_vendedor1_row"></div>
+        <div class="cadastro_vendedor1_lista_ordenada" >
             <ol>
                 <h1>Bem-Vindo!</h1>
                 <li>
@@ -39,8 +39,8 @@
                 <li>Celular ou E-mail. </li>
             </L>
         </div>
-        <div class="cadastro_vendedor_1_botao_salvar">
-          <button class="cadastro_vendedor_1_salvar">
+        <div class="cadastro_vendedor1_botao_salvar">
+          <button class="cadastro_vendedor1_salvar">
             <img src="../../image/admin/perfil_admin/v_icon.svg" alt="">
             <label>CONTINUAR</label>
           </button>
