76f7e86c197c6c9dfc0d174afffcf18b47215ed6
Davi <samuraiblack08@gmail.com>
2025-03-03 14:43:18 -0400

    Commit Add

--- pages/vendedor/cadastro_vendedor_1.php
+++ pages/vendedor/cadastro_vendedor_1.php
@@ -14,7 +14,7 @@
     <div class="nav_bar"></div>
     <div class="grid_bg" >
         <div class="titulo"><h1>CADASTRO DE VENDEDOR</h1></div>
-        <img src="../../image/geral/linha-divisoria-azul.png" alt="">
+        <div class="cadastro_row"></div>
         <div class="lista_ordenada" >
             <ol>
                 <h1>Bem-Vindo!</h1>
@@ -39,7 +39,14 @@
                 <li>Celular ou E-mail. </li>
             </L>
         </div>
-        <div class="botao"><h1>Iniciar Cadastro</h1></div>
+        <div class="cadastro_vendedor_1_botao_salvar">
+          <button class="cadastro_vendedor_1_salvar">
+            <img src="../../image/admin/perfil_admin/v_icon.svg" alt="">
+            <label>CONTINUAR</label>
+          </button>
+        </div>
+      </div>
+
     </div>
 </body>
 </html>
