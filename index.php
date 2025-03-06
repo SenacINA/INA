6a229c394a42f6a87ec8b392f7e8449531037a05
Filipe Simoes <filipe54098526@aluno.ms.senac.br>
2025-03-06 10:16:28 -0400

    fazer a pasta components

--- index.php
+++ index.php
@@ -5,37 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../../image/geral/icone_eaoquadrado.ico">
     <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>E ao Quadrado</title>
 </head>
 <body>
-<header class="nav style_nav shadow_nav">
-    <div class="grid-container style_nav">
-        <div class="item1">
-            <img id="imgLupa" src="./image/index/lupa.png" alt="">
-            <input type="text">
-        </div>
-        <div class="item2">
-            <button id="buttonLogo">
-                <a href="#">
-                <img href="#" id="imgLogo" src="./image/index/Logo.svg">
-                </a>
-            </button>
-        </div>
-        <div class="item3">
-            <a class="button" href="#">
-                <img src="./image/index/user.png" alt="Usuário">
-            </a>
-            <a class="button" href="#">
-                <img src="./image/index/carrinho.png" alt="Carrinho">
-            </a>
-            <a class="button" href="#">
-                <img src="./image/index/config.png" alt="Configurações">
-            </a>
-        </div>
-        
-    </div>
-</header>
-
+    <?php
+    include('./pages/geral/navbar.php');
+    ?>
 </body>
 </html>
 
