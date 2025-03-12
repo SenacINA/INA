9d89dc7bdf59d156f6f98fff709773e286b09db8
Davi <samuraiblack08@gmail.com>
2025-03-12 07:21:47 -0400

    Mudei o nome do css, pq n estava condizente

--- pages/admin/admin_carrossel.php
+++ pages/admin/admin_carrossel.php
@@ -9,7 +9,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../../css/admin/adicionar_propaganda.css">
+  <link rel="stylesheet" href="../../css/admin/admin_carrossel.css">
 </head>
 
 <body class="admin_carrossel_main_container">
