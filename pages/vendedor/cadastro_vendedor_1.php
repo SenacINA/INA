a5c63b924a7cea077e0ed252039decc874871d9b
Filipe Simoes <filipe54098526@aluno.ms.senac.br>
2025-03-12 07:26:38 -0400

    hr

--- pages/vendedor/cadastro_vendedor_1.php
+++ pages/vendedor/cadastro_vendedor_1.php
@@ -14,42 +14,42 @@
 <body>
     <div class="cadastro_vendedor1_nav_bar"></div>
     <div class="cadastro_vendedor1_background">
-    <div class="cadastro_vendedor1_grid" >   
-        <div class="cadastro_vendedor1_titulo">
-            <img class="cadastro_vendedor1_titulo_img" src="../../image/vendedor/cadastro_vendedor_1/cadastro_icon.png" alt="">
-            <h1>CADASTRO DE VENDEDOR</h1></div>
-        <div class="cadastro_vendedor1_row"></div>
-        <div class="cadastro_vendedor1_lista_ordenada" >
-            <ol>
-                <h1>Bem-Vindo!</h1>
-                <li>
-                    <h1>Fornecer informações e documentos</h1>
-                    <h2>Informações pessoais e comerciais relevantes para cumprir as medidas de identificação e verificação. </h2>
-                </li>
-                <li>
-                    <h1>Aprovação das credênciais</h1>
-                    <h2>Os dados fornecidos serão verificados através do nosso sistema de validação. Essa etapa auxilia na confiabilidade das vendas.</h2>
-                </li>
-                <li>
-                    <h1>Cadastro de produtos </h1>
-                    <h2>Após a verificação das informações e documentos (5 dias úteis). Faça o registro dos seus produtos. </h2>
-                </li>
-            </ol>
-            <L>
-                <h1>Dados necessários:</h1>
-                <li>Documento de identidade (RG) ou passaporte válido emitido pelo governo;</li>
-                <li>Declaração do banco ou cartão de crédito recente; </li>
-                <li>Cartão de crédito ou débito para cobrança;</li>
-                <li>Celular ou E-mail. </li>
-            </L>
+        <div class="cadastro_vendedor1_grid" >   
+            <div class="cadastro_vendedor1_titulo">
+                <img class="cadastro_vendedor1_titulo_img" src="../../image/vendedor/cadastro_vendedor_1/cadastro_icon.png" alt="">
+                <h1>CADASTRO DE VENDEDOR</h1></div>
+            <hr>
+            <div class="cadastro_vendedor1_lista_ordenada" >
+                <ol>
+                    <h1>Bem-Vindo!</h1>
+                    <li>
+                        <h1>Fornecer informações e documentos</h1>
+                        <h2>Informações pessoais e comerciais relevantes para cumprir as medidas de identificação e verificação. </h2>
+                    </li>
+                    <li>
+                        <h1>Aprovação das credênciais</h1>
+                        <h2>Os dados fornecidos serão verificados através do nosso sistema de validação. Essa etapa auxilia na confiabilidade das vendas.</h2>
+                    </li>
+                    <li>
+                        <h1>Cadastro de produtos </h1>
+                        <h2>Após a verificação das informações e documentos (5 dias úteis). Faça o registro dos seus produtos. </h2>
+                    </li>
+                </ol>
+                <L>
+                    <h1>Dados necessários:</h1>
+                    <li>Documento de identidade (RG) ou passaporte válido emitido pelo governo;</li>
+                    <li>Declaração do banco ou cartão de crédito recente; </li>
+                    <li>Cartão de crédito ou débito para cobrança;</li>
+                    <li>Celular ou E-mail. </li>
+                </L>
+            </div>
+            <div class="cadastro_vendedor1_botao_salvar">
+            <button class="cadastro_vendedor1_salvar">
+                <img src="../../image/vendedor/cadastro_vendedor_1/v_icon.svg" alt="">
+                <label>CONTINUAR</label>
+            </button>
+            </div>
         </div>
-        <div class="cadastro_vendedor1_botao_salvar">
-          <button class="cadastro_vendedor1_salvar">
-            <img src="../../image/vendedor/cadastro_vendedor_1/v_icon.svg" alt="">
-            <label>CONTINUAR</label>
-          </button>
-        </div>
-      </div>
       </div>
     </div>
 
