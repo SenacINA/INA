0c2a574755ab1311852e195965c4b4dc8d8e093c
LuisCunha05 <luisf.cunha05@gmail.com>
2025-03-12 11:10:29 -0400

    wip: responsividade 1

--- pages/cliente/carrinho_vazio.php
+++ pages/cliente/carrinho_vazio.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E ao Quadrado</title>
     <link rel="icon" type="image/x-icon" href="../../image/geral/icone_eaoquadrado.ico">
-    <link rel="stylesheet" href="../../css/index.css">
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="../../css/cliente/carrinho_vazio.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -15,50 +14,32 @@
 
 </head>
 
-<body>
-    <header class="nav style_nav shadow_nav">
-        <div class="grid-container style_nav">
-            <div class="item1">
-                <img id="imgLupa" src="../../image/index/lupa.png" alt="">
-                <input type="text">
-            </div>
-            <div class="item2">
-                <button id="buttonLogo">
-                    <a href="#">
-                        <img href="#" id="imgLogo" src="../../image/index/logo.svg">
-                    </a>
-                </button>
-            </div>
-            <div class="item3">
-                <a class="button" href="#">
-                    <img src="../../image/index/user.png" alt="Usuário">
-                </a>
-                <a class="button" href="#">
-                    <img src="../../image/index/carrinho.png" alt="Carrinho">
-                </a>
-                <a class="button" href="#">
-                    <img src="../../image/index/config.png" alt="Configurações">
-                </a>
-            </div>
+<body class="carrinho_vazio_body">
 
-        </div>
-    </header>
-    <main>
-        <div class="carrinho_vazio">
-            <img src="../../image/carrinho/carrinho.svg">
-            <p class="text">Carrinho</p>
+    <main class="carrinho_vazio_main">
+        <div class="carrinho_vazio_nav">
+            <div class="carrinho_vazio_nav_item">
+                <img src="../../image/carrinho/carrinho.svg">
+                <p class="text">Carrinho</p>
+            </div>
             <hr class="divisoria">
-            <img src="../../image/carrinho/identificacao.svg">
-            <p class="text-2">Identificação</p>
+            <div class="carrinho_vazio_nav_item">
+                <img src="../../image/carrinho/identificacao.svg">
+                <p class="text-2">Identificação</p>
+            </div>
             <hr class="divisoria">
-            <img src="../../image/carrinho/conclusao.svg">
-            <p class="text-2">Conclusão</p>
+            <div class="carrinho_vazio_nav_item">
+                <img src="../../image/carrinho/conclusao.svg">
+                <p class="text-2">Conclusão</p>
+            </div>
             <hr class="divisoria">
-            <img src="../../image/carrinho/confirmacao.svg">
-            <p class="text-2">Confirmação</p>
+            <div class="carrinho_vazio_nav_item">
+                <img src="../../image/carrinho/confirmacao.svg">
+                <p class="text-2">Confirmação</p>
+            </div>
         </div>
 
-        <div class="quadrado">
+        <div class="carrinho_vazio_conteudo">
             <div class="text_info">
                 <h1 class="text_ produto"><b>Produto</b></h1>
                 <h1 class="text_"><b>Quantidade</b></h1>
