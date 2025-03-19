c5e102667cd03ba378d526529f244dfbceb0ec33
Filipe Simoes <filipe54098526@aluno.ms.senac.br>
2025-03-19 10:37:07 -0400

    arruma o resto enzo

--- pages/vendedor/cadastro_vendedor_2.php
+++ pages/vendedor/cadastro_vendedor_2.php
@@ -23,122 +23,129 @@
   ?>
 
   <div class="cadastro_vendedor_2_grid_bg">
-    <div class="cadastro_vendedor2_titulo">
-      <div class="cadastro_vendedor2_titulo_img">
-        <img src="../../image/vendedor/cadastro_vendedor_1/cadastro_icon.png" alt="">
-        <h1>CADASTRO DE VENDEDOR</h1>
+    <div class="cadastro_vendedor2_content">
+      <div class="cadastro_vendedor2_titulo">
+        <div class="cadastro_vendedor2_titulo_img">
+          <img src="../../image/vendedor/cadastro_vendedor_1/cadastro_icon.png" alt="">
+          <h1>CADASTRO DE VENDEDOR</h1>
+        </div>
+        <hr>
       </div>
-      <hr>
-    </div>
-    <div class="cadastro_vendedor_2_grid_conteudo">
-      <div class="cadastro_vendedor_2_grid_conteudo_1">
-        <!-- Local da Empresa -->
-        <label for="local_da_empresa">Local da Empresa</label> <br>
-        <select name="local_da_empresa" id="local_da_empresa" class="base_input">
-          <option value="" disabled selected>Selecione a cidade</option>
-          <option value="aquidauana">Aquidauana</option>
-          <option value="campo_grande">Campo Grande</option>
-          <option value="chapadão_do_sul">Chapadão do Sul</option>
-          <option value="corumbá">Corumbá</option>
-          <option value="coxim">Coxim</option>
-          <option value="dourados">Dourados</option>
-          <option value="maracaju">Maracaju</option>
-          <option value="nova_andradina">Nova Andradina</option>
-          <option value="paranaíba">Paranaíba</option>
-          <option value="ponta_pora">Ponta Porã</option>
-          <option value="porto_murtinho">Porto Murtinho</option>
-          <option value="sidrolândia">Sidrolândia</option>
-          <option value="três_lagoas">Três Lagoas</option>
-        </select>
-        <h1>Se você não tiver uma empresa, informe o estado de sua residência.</h1>
-
-        <!-- CEP -->
-        <label for="cep">CEP</label> <br>
-        <input type="text" name="cep" id="cep" class="base_input">
-        <br>
-
-        <div class="cadastro_vendedor_2_conteudo_endereco1">
-          <!-- Logradouro -->
-          <div class="cadastro_vendedor_2_logradouro">
-            <label for="logradouro">Logradouro</label> <br>
-            <input type="text" name="logradouro" id="logradouro" class="base_input">
+      <div class="cadastro_vendedor_2_grid_conteudo">
+        <div class="cadastro_vendedor_2_grid_conteudo_1">
+          <!-- Local da Empresa -->
+          <div>
+            <label for="local_da_empresa">Local da Empresa</label> <br>
+            <select name="local_da_empresa" id="local_da_empresa" class="base_input">
+              <option value="" disabled selected>Selecione a cidade</option>
+              <option value="aquidauana">Aquidauana</option>
+              <option value="campo_grande">Campo Grande</option>
+              <option value="chapadão_do_sul">Chapadão do Sul</option>
+              <option value="corumbá">Corumbá</option>
+              <option value="coxim">Coxim</option>
+              <option value="dourados">Dourados</option>
+              <option value="maracaju">Maracaju</option>
+              <option value="nova_andradina">Nova Andradina</option>
+              <option value="paranaíba">Paranaíba</option>
+              <option value="ponta_pora">Ponta Porã</option>
+              <option value="porto_murtinho">Porto Murtinho</option>
+              <option value="sidrolândia">Sidrolândia</option>
+              <option value="três_lagoas">Três Lagoas</option>
+            </select>
+            <h1>Se você não tiver uma empresa, informe o estado de sua residência.</h1>
           </div>
 
-          <!-- Número -->
-          <div class="cadastro_vendedor_2_numero">
-            <label for="numero">Número</label> <br>
-            <input type="text" name="numero" id="numero" class="base_input">
+          <!-- CEP -->
+          <label for="cep">CEP</label> <br>
+          <input type="text" name="cep" id="cep" class="base_input">
+          <br>
+
+          <div class="cadastro_vendedor_2_conteudo_endereco1">
+            <!-- Logradouro -->
+            <div class="cadastro_vendedor_2_logradouro">
+              <label for="logradouro">Logradouro</label> <br>
+              <input type="text" name="logradouro" id="logradouro" class="base_input">
+            </div>
+
+            <!-- Número -->
+            <div class="cadastro_vendedor_2_numero">
+              <label for="numero">Número</label> <br>
+              <input type="text" name="numero" id="numero" class="base_input">
+            </div>
+          </div>
+
+          <!-- Nome / Razão Social -->
+          <label for="nome_razao_social">Nome / Razão Social</label> <br>
+          <input type="text" name="nome_razao_social" id="nome_razao_social" class="base_input">
+          <br>
+
+          <div class="cadastro_vendedor_2_conteudo_endereco2">
+            <!-- CPF / CNPJ -->
+            <div class="cadastro_vendedor_2_cpf_cnpj">
+              <label for="cpf_cnpj">CPF / CNPJ</label> <br>
+              <input type="text" name="cpf_cnpj" id="cpf_cnpj" class="base_input">
+            </div>
+
+            <!-- RG -->
+            <div class="cadastro_vendedor_2_rg">
+              <label for="rg">RG</label> <br>
+              <input type="text" name="rg" id="rg" class="base_input">
+            </div>
           </div>
         </div>
 
-        <!-- Nome / Razão Social -->
-        <label for="nome_razao_social">Nome / Razão Social</label> <br>
-        <input type="text" name="nome_razao_social" id="nome_razao_social" class="base_input">
-        <br>
-
-        <div class="cadastro_vendedor_2_conteudo_endereco2">
-          <!-- CPF / CNPJ -->
-          <div class="cadastro_vendedor_2_cpf_cnpj">
-            <label for="cpf_cnpj">CPF / CNPJ</label> <br>
-            <input type="text" name="cpf_cnpj" id="cpf_cnpj" class="base_input">
+        <div class="cadastro_vendedor_2_grid_conteudo_2">
+          <!-- E-mail -->
+          <div>
+            <label for="email">E-mail</label> <br>
+            <input type="email" name="email" id="email" class="base_input">
+            <h1>O código de confirmação será enviado para esse E-mail. </h1>
           </div>
+          
 
-          <!-- RG -->
-          <div class="cadastro_vendedor_2_rg">
-            <label for="rg">RG</label> <br>
-            <input type="text" name="rg" id="rg" class="base_input">
-          </div>
-        </div>
-      </div>
+          <!-- Senha -->
+          <label for="senha">Senha</label> <br>
+          <input type="password" name="senha" id="senha" class="base_input">
+          <br>
 
-      <div class="cadastro_vendedor_2_grid_conteudo_2">
-        <!-- E-mail -->
-        <label for="email">E-mail</label> <br>
-        <input type="email" name="email" id="email" class="base_input">
-        <h1>O código de confirmação será enviado para esse E-mail. </h1>
+          <!-- Confirmar Senha -->
+          <label for="confirmar_senha">Confirmar Senha</label> <br>
+          <input type="password" name="confirmar_senha" id="confirmar_senha" class="base_input">
+          <br>
 
-        <!-- Senha -->
-        <label for="senha">Senha</label> <br>
-        <input type="password" name="senha" id="senha" class="base_input">
-        <br>
+          <!-- Categoria de Produtos -->
+          <label for="categoria_produtos">Categoria de Produtos</label> <br>
+          <select name="categoria_produtos" id="categoria_produtos" class="base_input">
+            <option value="opc1">Opção 1</option>
+            <option value="opc2">Opção 2</option>
+            <option value="opc3">Opção 3</option>
+            <option value="opc4">Opção 4</option>
+            <option value="opc5">Opção 5</option>
 
-        <!-- Confirmar Senha -->
-        <label for="confirmar_senha">Confirmar Senha</label> <br>
-        <input type="password" name="confirmar_senha" id="confirmar_senha" class="base_input">
-        <br>
+          </select>
+          <br>
 
-        <!-- Categoria de Produtos -->
-        <label for="categoria_produtos">Categoria de Produtos</label> <br>
-        <select name="categoria_produtos" id="categoria_produtos" class="base_input">
-          <option value="opc1">Opção 1</option>
-          <option value="opc2">Opção 2</option>
-          <option value="opc3">Opção 3</option>
-          <option value="opc4">Opção 4</option>
-          <option value="opc5">Opção 5</option>
+          <div class="cadastro_vendedor_2_conteudo_telefone">
+            <!-- Telefone 1 -->
+            <div class="cadastro_vendedor_2_telefone1">
+              <label for="telefone1">Telefone 1</label> <br>
+              <input type="tel" name="telefone1" id="telefone1" class="base_input">
+              <br>
+            </div>
 
-        </select>
-        <br>
-
-        <div class="cadastro_vendedor_2_conteudo_telefone">
-          <!-- Telefone 1 -->
-          <div class="cadastro_vendedor_2_telefone1">
-            <label for="telefone1">Telefone 1</label> <br>
-            <input type="tel" name="telefone1" id="telefone1" class="base_input">
-            <br>
-          </div>
-
-          <!-- Telefone 2 -->
-          <div class="cadastro_vendedor_2_telefone2">
-            <label for="telefone2">Telefone 2</label> <br>
-            <input type="tel" name="telefone2" id="telefone2" class="base_input">
-            <br>
+            <!-- Telefone 2 -->
+            <div class="cadastro_vendedor_2_telefone2">
+              <label for="telefone2">Telefone 2</label> <br>
+              <input type="tel" name="telefone2" id="telefone2" class="base_input">
+              <br>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="cadastro_vendedor_2_botao" onclick="pag('vendedor/perfil_vendedor')">
-      <img src="../../image/vendedor/cadastro_vendedor_1/v_icon.svg">
-      <h1>Finalizar Cadastro</h1>
+      <div class="cadastro_vendedor_2_botao botao_input" onclick="pag('vendedor/perfil_vendedor')">
+        <img src="../../image/vendedor/cadastro_vendedor_1/v_icon.svg">
+        <h1>FINALIZAR</h1>
+      </div>
     </div>
   </div>
   <?php
