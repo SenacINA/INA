164dbe88bc463988a4017b56a8200745bcccf56f
Hawstyyy <hawstyyy@proton.me>
2025-03-19 11:10:10 -0400

    salvando ultimas alterações

--- pages/vendedor/editar_perfil_vendedor.php
+++ pages/vendedor/editar_perfil_vendedor.php
@@ -1,3 +1,31 @@
+<?php
+function gerarProdutoCards($quantidade)
+{
+  for ($i = 0; $i < $quantidade; $i++) {
+    echo "
+        <div class='index_body_produto_card'>
+            <div class='index_body_desconto'>
+                <span>20% OFF</span>
+            </div>
+            <div class='index_body_imagem_produto'>
+                <img src='https://placehold.co/200' alt=''>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pellentesque vel nulla quis dapibus. Donec purus purus, maximus eget sollicitudin a, maximus vitae enim. Maecenas facilisis elementum porta. Nullam sagittis, orci eu gravida consequat, turpis diam maximus lectus, in aliquam libero tortor a lorem. Donec malesuada purus at ligula consequat dignissim. Sed quis mollis sem. In laoreet metus maximus velit venenatis eleifend eu vel dui. Vivamus dignissim velit vel nisi lobortis aliquam</p>
+            </div>
+            <div class='index_body_estrela_valor'>
+                <div class='index_body_estrela_produto'>
+                    <p>★★★★★</p>
+                    <h4>(12)</h4>
+                </div>
+                <div class='index_body_valor_produto'>
+                    <p id='indexBodyValorAntigo'>R$1000</p>
+                    <p id='indexBodyValorProduto'>R$800</p>
+                </div>
+            </div>
+        </div>";
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -11,6 +39,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="../../css/style.css">
+  <link rel="stylesheet" href="../../css/index.css">
   <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
@@ -19,7 +48,7 @@
   <!-- arruamar o nome das class -->
 
   <?php
-    include_once('../../pages/geral/navbar.php');
+  include_once('../../pages/geral/navbar.php');
   ?>
   <main class="quadrado_vendedor">
     <div class="mini_perfil_vendedor">
@@ -53,21 +82,12 @@
         <p class="text_about">Lorem ipsum dolor sit amet. Non quidem earum ut facilis deserunt et voluptatem praesentium et error distinctio. In doloremque minus et harum ducimus hic omnis sapiente ut perferendis perferendis. Quo distinctio consequatur est consequuntur repellendus eos fugiat accusantium quo quod eius et nesciunt temporibus. At recusandae asperiores et nisi laborum id sint suscipit et asperiores consequatur est molestiae Quis qui dolorem vitae. At dolorum quos non omnis internos et quos quis.</p>
       </div>
       <div class="contatos_container">
-        <img src="../../image/vendedor/editar_perfil/icon_contatos_vendedor.svg" class="icon_contatos_vendedor">
-        <h1 class="main_text_contatos">Contatos</h1>
         <div class="contatos_vendedor">
           <img src="../../image/vendedor/editar_perfil/icon_instagram_vendedor.svg" class="icon_instagram_vendedor">
           <a href="#" class="instagram_vendedor">my.thudergames</a>
+          <hr class="linha_vertical_mini">
           <img src="../../image/vendedor/editar_perfil/icon_facebook_vendedor.svg" class="icon_facebook_vendedor">
           <a href="#" class="facebook_vendedor">thundergames</a>
-          <img src="../../image/vendedor/editar_perfil/icon_x_vendedor.png" class="icon_x_vendedor">
-          <a href="#" class="x_vendedor">thundergames_</a>
-          <img src="../../image/vendedor/editar_perfil/icon_linkedin_vendedor.png" class="icon_linkedin_vendedor">
-          <a href="#" class="linkedin_vendedor">thundergames</a>
-          <img src="../../image/vendedor/editar_perfil/icon_youtube_vendedor.png" class="icon_youtube_vendedor">
-          <a href="#" class="youtube_vendedor">Thunder Games</a>
-          <img src="../../image/vendedor/editar_perfil/icon_tiktok_vendedor.png" class="icon_tiktok_vendedor">
-          <a href="#" class="tiktok_vendedor">thunder.games</a>
         </div>
       </div>
     </div>
@@ -95,7 +115,10 @@
         <button type="button" class="redefinir_email_vendedor">Redefinir email</button>
       </div>
       <div class="redes_sociais_vendedor">
-        <p class="main_text_redes">Redes Sociais:</p>
+        <div class="redes_text">
+          <hr class="linha_vertical">
+          <p class="main_text_redes">Redes Sociais:</p>
+        </div>
         <div class="redes_edit">
           <img src="../../image/vendedor/editar_perfil/icon_instagram_vendedor.svg" alt="instagram"> <a href="#" class="link_instagram">my.thudergames </a>
           <img src="../../image/vendedor/editar_perfil/icon_facebook_vendedor.svg" alt="facebook"> <a href="#" class="link_facebook">thundergames</a>
@@ -105,21 +128,48 @@
     </div>
     <div class="edit_foto">
       <div class="foto_container">
-        <p class="text_pfp">Imagem de Perfil:</p>
-        <input type="file" name="foto_vendedor" id="fotoVendedor" class="btn_foto" />
-        <img src="../../image/vendedor/editar_perfil/pfp_vendedor.png" alt="">
+        <div class="foto_text">
+          <hr class="linha_vertical">
+          <p class="text_pfp">Imagem de Perfil:</p>
+        </div>
+        <button class="img_container">
+          <img src="../../image/vendedor/editar_perfil/pfp_vendedor.png">
+        </button>
         <p class="warn">As dimensões recomendadas são: 400 x 400 pixels.</p>
       </div>
       <div class="banner_container">
-        <p class="text_banner">Imagem de Banner:</p>
-        <input type="file" name="foto_vendedor" id="fotoVendedor" class="btn_banner" />
-        <img src="../../image/vendedor/editar_perfil/banner_vendedor_mini_perfil.png" class="banner_vendedor_forms" alt="">
+        <div class="banner_text">
+          <hr class="linha_vertical">
+          <p class="text_banner">Imagem de Banner:</p>
+        </div>
+        <button class="img_container">
+          <img src="../../image/vendedor/editar_perfil/banner_vendedor_mini_perfil.png" class="banner_vendedor_forms">
+        </button>
         <p class="warn">As dimensões recomendadas são: 1500 x 500 pixels.</p>
       </div>
     </div>
+    <div class="destaques">
+      <div class="destaques_text">
+        <hr class="linha_vertical">
+        <img src="../../image/vendedor/editar_perfil/icon_produtos_vendedor.svg" class="icon_destaques_vendedor">
+        <p class="destaques_main_text">Destaques</p>
+      </div>
+      <div class="destaques_container">
+        <?php gerarProdutoCards(3); ?>
+      </div>
+    </div>
+    <div class="botoes">
+      <button class="salvar">
+        <img src="../../image/geral/botoes/v_icon_branco.svg">Salvar
+      </button>
+      <button class="cancelar">
+        <img src="../../image/geral/botoes/x_icon_vermelho.svg">Cancelar
+      </button>
+    </div>
   </main>
-  <?php 
-      include_once('../../pages/geral/footer.php');
+  <?php
+  include_once('../../pages/geral/footer.php');
   ?>
 </body>
+
 </html>
