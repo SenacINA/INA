b923dcfe9d838a8ddfe5e8ba97e1809f0999cdb7
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2025-03-20 20:02:39 -0400

    tinha q ser um Enzo

--- pages/vendedor/editar_perfil_vendedor.php
+++ pages/vendedor/editar_perfil_vendedor.php
@@ -121,8 +121,15 @@ function gerarProdutoCards($quantidade)
         </div>
       </form>
       <div class="botoes_redefinir">
-        <button type="button" class='redefinir_senha_vendedor'>Redefinir senha</button>
-        <button type="button" class="redefinir_email_vendedor">Redefinir email</button>
+        <button type="button" class='redefinir_senha_vendedor'>
+          <img src="../../image/geral/seta_botao_branco.svg" alt="">
+          Redefinir senha
+
+        </button>
+        <button type="button" class="redefinir_email_vendedor">
+          <img src="../../image/geral/seta_botao_branco.svg" alt="">
+          Redefinir email
+        </button>
       </div>
       <div class="redes_sociais_vendedor">
         <div class="redes_text">
@@ -133,7 +140,10 @@ function gerarProdutoCards($quantidade)
           <img src="../../image/vendedor/editar_perfil/icon_instagram_vendedor.svg" alt="instagram"> <a href="#" class="link_instagram">my.thudergames </a>
           <img src="../../image/vendedor/editar_perfil/icon_facebook_vendedor.svg" alt="facebook"> <a href="#" class="link_facebook">thundergames</a>
         </div>
-        <button class="botao_edit">Editar</button>
+        <button class="botao_edit">
+          <img src="../../image/geral/caneta_branca_icon.svg" alt="">
+          Editar
+        </button>
       </div>
     </div>
     <div class="edit_foto">
