d67726fabb2e391ba6b627b82f8b6fd85414ba30
eliasasa <eliasnetoneto1@hotmail.com>
2025-04-01 19:17:31 -0400

    senac

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"> 
     <title>E ao Quadrado</title>
     <link rel="icon" type="image/x-icon" href="./image/geral/icone_eaoquadrado.ico">    
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <script src="js/geral/base.js"></script>
 </head>
 <body>
@@ -17,56 +17,10 @@
     include_once('./pages/geral/navbar.php');
     ?>
 <div class="index_body_main_container">
-    <div class="carrossel">
-        <div class="carrossel_content">
-                <button class="carrossel_but back">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19"/>
-                    </svg>
-                </button>
-            <div class="carousel-container">
-                
-                <div class="carousel-wrapper">
-                    <img id="carrossel_image" src="./image/index/carrosselConsole.png" alt="Imagem Carrossel">
-                </div>
-
-                
-            </div>
-            <button class="carrossel_but forward">                    
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                         <path fill="currentColor" d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19"/>
-                    </svg> 
-                </button>
-        </div>
-        <div class="carrossel_nav">
-            <button class="active" onclick="currentSlide(0)"></button>
-            <button onclick="currentSlide(1)"></button>
-            <button onclick="currentSlide(2)"></button>
-            <button onclick="currentSlide(3)"></button>
-        </div>
-        <div class="categorias_nav">
-            <div class="categorias_block" onclick="pag('cliente/categoria',0)">
-                <img src="./image/index/carrosselHardware.png" alt="">
-                <p>Hardware</p>
-            </div>
-            <div class="categorias_block" onclick="pag('cliente/categoria',0)">
-                <img src="./image/index/carrosselPerifericos.png" alt="">
-                <p>Periféricos</p>
-            </div>
-            <div class="categorias_block" onclick="pag('cliente/categoria',0)">
-                <img src="./image/index/carrosselEscritorio.png" alt="">
-                <p>Escritório</p>
-            </div>
-            <div class="categorias_block" onclick="pag('cliente/categoria',0)">
-                <img src="./image/index/carrosselCelulares.png" alt="">
-                <p>Celulares</p>
-            </div>
-            <div class="categorias_block" onclick="pag('cliente/categoria',0)">
-                <img src="./image/index/carrosselEletro.png" alt="">
-                <p>Eletrodomésticos</p>
-            </div>
-        </div>
-    </div>
+    <?php
+        include_once('./pages/geral/carrossel.php');
+        Carrossel(1);
+    ?>
     <div class="index_body_bg_container">
         <div class="index_body_content_container">
             <div class="index_body_ad_box">
@@ -148,5 +102,6 @@
     ?>
 </div>
 <script src="js/geral/index.js"></script>
+<script src='js/cliente/carrossel.js' data-isindex="1"></script>
 </body>
 </html>
