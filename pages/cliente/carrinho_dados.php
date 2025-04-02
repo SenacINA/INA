74b26015d6354a4e74174f4a9dad8881bba470af
Hawstyyy <hawstyyy@proton.me>
2025-04-02 10:57:28 -0400

    Realizando alterações solicitadas e fazendo a responsividade da tela

--- pages/cliente/carrinho_dados.php
+++ pages/cliente/carrinho_dados.php
@@ -1,31 +1,48 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/x-icon" href="../../image/geral/icone_eaoquadrado.ico">
   <title>E ao Quadrado</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com"> 
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-  <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"> 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../../css/cliente/carinho_dados.css">
   <link rel="stylesheet" href="../../css/style.css">
   <script src="../../js/geral/base.js"></script>
 </head>
+
 <body class="fundo">
   <!-- fazer responsividade -->
   <!-- arrumar o nome das class -->
-   
+
   <?php
-    include_once('../../pages/geral/navbar.php');
+  include_once('../../pages/geral/navbar.php');
   ?>
 
   <div class="carrinho_dados">
-    <img src="../../image/cliente/carrinho_dados/carrinho_top.png" class="carrinho_top">
+    <div class="carrinho_nav">
+      <div class="carrinho_nav_item">
+        <img src="../../image/carrinho/carrinho_cinza.svg">
+        <span>Carrinho</span>
+      </div>
+      <hr class="carrinho_divisoria">
+      <div class="carrinho_nav_item">
+        <img src="../../image/carrinho/identificacao_selected.svg">
+        <span>Identificação</span>
+      </div>
+      <hr class="carrinho_divisoria">
+      <div class="carrinho_nav_item">
+        <img src="../../image/carrinho/pagamento.svg">
+        <span>Pagamento</span>
+      </div>
+    </div>
     <div class="forms_dados">
       <div class="main_container_carrinho_dados">
         <p class="main_text_carrinho_dados">Identificação</p>
-        <img src="../../image/cliente/carrinho_dados/separador_carrinho.png" class="separador_carrinho">
+        <hr class="separador_carrinho">
       </div>
       <div class="container_forms">
         <form action="" class="forms_carrinho">
@@ -45,8 +62,6 @@
           <input type="text" name="telefone" id="telefone" class="base_input">
           <label for="email">Email:</label>
           <input type="text" name="email" id="email" class="base_input">
-        </form>
-        <form action="" class="dados_adicionais">
           <label for="ponto">Ponto de referência(opcional):</label>
           <input type="text" name="ponto" id="ponto" class="base_input">
           <label for="numeroCasa">Número da casa:</label>
@@ -60,13 +75,156 @@
           </select>
         </form>
         <div class="botoes_carrinho">
-          <button class="salvar_carrinho" onclick="pag('cliente/carrinho_pagamentos')"><img src="../../image/geral/confirm_botao.svg">Salvar</button>
+          <button class="salvar_carrinho" onclick="pag('cliente/carrinho_pagamentos')"><img src="../../image/geral/botoes/v_branco_icon.svg">Salvar</button>
+        </div>
+        <div class="informacoes_salvas">
+          <div class="enderecos_salvos_main">
+            <p class="enderecos_salvos_main_text">Endereços Salvos</p>
+          </div>
+          <forms action="" class="enderecos_salvos_container">
+            <div class="info_container">
+              <input type="radio" name="endereco">
+              <div class="text_info">
+                <div class="text">
+                  <h3 class="endereco_info">Endereço</h3>
+                  <p class="endereco_info_adicional">Nome, Número da casa, Cidade, CEP</p>
+                </div>
+                <div class="botoes_info">
+                  <button class="edit_info">
+                    <img src="../../image/geral/icons/caneta_carolina_icon.svg">
+                  </button>
+                  <button class="remove_info">
+                    <img src="../../image/geral/icons/lixo_vermelho_icon.svg">
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="info_container">
+              <input type="radio" name="endereco">
+              <div class="text_info">
+                <div class="text">
+                  <h3 class="endereco_info">Endereço</h3>
+                  <p class="endereco_info_adicional">Nome, Número da casa, Cidade, CEP</p>
+                </div>
+                <div class="botoes_info">
+                  <button class="edit_info">
+                    <img src="../../image/geral/icons/caneta_carolina_icon.svg">
+                  </button>
+                  <button class="remove_info">
+                    <img src="../../image/geral/icons/lixo_vermelho_icon.svg">
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="info_container">
+              <input type="radio" name="endereco">
+              <div class="text_info">
+                <div class="text">
+                  <h3 class="endereco_info">Endereço</h3>
+                  <p class="endereco_info_adicional">Nome, Número da casa, Cidade, CEP</p>
+                </div>
+                <div class="botoes_info">
+                  <button class="edit_info">
+                    <img src="../../image/geral/icons/caneta_carolina_icon.svg">
+                  </button>
+                  <button class="remove_info">
+                    <img src="../../image/geral/icons/lixo_vermelho_icon.svg">
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="info_container">
+              <input type="radio" name="endereco">
+              <div class="text_info">
+                <div class="text">
+                  <h3 class="endereco_info">Endereço</h3>
+                  <p class="endereco_info_adicional">Nome, Número da casa, Cidade, CEP</p>
+                </div>
+                <div class="botoes_info">
+                  <button class="edit_info">
+                    <img src="../../image/geral/icons/caneta_carolina_icon.svg">
+                  </button>
+                  <button class="remove_info">
+                    <img src="../../image/geral/icons/lixo_vermelho_icon.svg">
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="info_container">
+              <input type="radio" name="endereco">
+              <div class="text_info">
+                <div class="text">
+                  <h3 class="endereco_info">Endereço</h3>
+                  <p class="endereco_info_adicional">Nome, Número da casa, Cidade, CEP</p>
+                </div>
+                <div class="botoes_info">
+                  <button class="edit_info">
+                    <img src="../../image/geral/icons/caneta_carolina_icon.svg">
+                  </button>
+                  <button class="remove_info">
+                    <img src="../../image/geral/icons/lixo_vermelho_icon.svg">
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="info_container">
+              <input type="radio" name="endereco">
+              <div class="text_info">
+                <div class="text">
+                  <h3 class="endereco_info">Endereço</h3>
+                  <p class="endereco_info_adicional">Nome, Número da casa, Cidade, CEP</p>
+                </div>
+                <div class="botoes_info">
+                  <button class="edit_info">
+                    <img src="../../image/geral/icons/caneta_carolina_icon.svg">
+                  </button>
+                  <button class="remove_info">
+                    <img src="../../image/geral/icons/lixo_vermelho_icon.svg">
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="info_container">
+              <input type="radio" name="endereco">
+              <div class="text_info">
+                <div class="text">
+                  <h3 class="endereco_info">Endereço</h3>
+                  <p class="endereco_info_adicional">Nome, Número da casa, Cidade, CEP</p>
+                </div>
+                <div class="botoes_info">
+                  <button class="edit_info">
+                    <img src="../../image/geral/icons/caneta_carolina_icon.svg">
+                  </button>
+                  <button class="remove_info">
+                    <img src="../../image/geral/icons/lixo_vermelho_icon.svg">
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="info_container">
+              <input type="radio" name="endereco">
+              <div class="text_info">
+                <div class="text">
+                  <h3 class="endereco_info">Endereço</h3>
+                  <p class="endereco_info_adicional">Nome, Número da casa, Cidade, CEP</p>
+                </div>
+                <div class="botoes_info">
+                  <button class="edit_info">
+                    <img src="../../image/geral/icons/caneta_carolina_icon.svg">
+                  </button>
+                  <button class="remove_info">
+                    <img src="../../image/geral/icons/lixo_vermelho_icon.svg">
+                  </button>
+                </div>
+              </div>
+            </div>
+          </forms>
         </div>
       </div>
     </div>
   </div>
-  <?php 
-      include_once('../../pages/geral/footer.php');
+  <?php
+  include_once('../../pages/geral/footer.php');
   ?>
 </body>
 
