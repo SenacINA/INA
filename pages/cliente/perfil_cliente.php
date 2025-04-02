258cd692cb839608fcd9898322e0cf7ecf391990
Filipe Simoes <filipe54098526@aluno.ms.senac.br>
2025-04-02 09:40:08 -0400

    arrumei o margin-top da pág

--- pages/cliente/perfil_cliente.php
+++ pages/cliente/perfil_cliente.php
@@ -33,12 +33,13 @@ function gerarProdutoCards($quantidade)
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/x-icon" href="../../image/geral/icone_eaoquadrado.ico">
   <title>E ao Quadrado</title>
+  <link rel="stylesheet" href="../../css/style.css">
   <link rel="stylesheet" href="../../css/cliente/perfil_cliente.css">
   <link rel="shortcut icon" href="../../image/geral/icone_eaoquadrado.ico" type="image/x-icon">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../../css/style.css">
+  
   <script src="../../js/geral/base.js"></script>
 </head>
 <body>
