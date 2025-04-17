e7cc23ac6b92643f8404de197e550c2446c5d191
eliasasa <eliasnetoneto1@hotmail.com>
2025-04-17 10:43:41 -0400

    elias

--- config/routes.php
+++ config/routes.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "hello!??";
+    return [
+        '/' => './app/views/geral/home.php',
+        '/contato' => '/../app/views/geral/contato.php',
+        '404' => '/../app/views/geral/error.php',
+    ];
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-    session_start();
-    header('Location: ./app/views/geral/home.php');
+    // session_start();
+
+    $routes = require __DIR__ . '/config/routes.php';
+    
+
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    // echo $uri;
+    // echo $routes['/'];
+
+    if ($uri === '/INA/index.php') {
+
+        header("Location: " . $routes['/']);
+        
+    }
+
+    else {
+        http_response_code(404);
+        header("Location: ./app/views/geral/error.php");
+    }
