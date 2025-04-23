d7f4bede0550a4d485f9aa7e504bbf7263a4b84b
Vyenvyy <alexyvne@gmail.com>
2025-04-23 10:56:12 -0400

    Att Atualizar Usuário

--- app/views/admin/atualizar_usuario.php
+++ app/views/admin/atualizar_usuario.php
@@ -5,15 +5,15 @@
   $css = ["/css/admin/atualizar_usuario.css"];
   require_once('../../../utils/head.php');
 ?>
+
+<!-- FAZER A RESPONSIVIDADE -->
+
 <body> 
-  <!-- Até 375px -->
-  <!-- Caminho de Icon Correto -->
-  
-  <?php
-    include_once("$PATH_COMPONENTS/php/navbar.php");
-  ?>
+  <?php include_once("$PATH_COMPONENTS/php/navbar.php"); ?>
+
   <main>
     <div class="atualizar_usuario_body">
+      
       <div class="atualizar_usuario_titulo">
         <div class="atualizar_usuario_text_titulo">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/atualizar_icon.svg" alt="">
@@ -24,17 +24,19 @@
 
       <div class="bg_mobile_1"></div>
       <div class="bg_mobile_2"></div>
-     
+
       <div class="atualizar_usuario_grid_conteudo">
+        
+        <!-- Alternar Permissões -->
         <div class="atualizar_usuario_text" id="atualizar_usuario_text_1"> 
           <hr class="atualizar_usuario_vertical">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/lista_icon.svg" alt="">
           <h1 class="atualizar_usuario_text">Alternar Permissões</h1>
         </div>
 
+        <!-- Pesquisa -->
         <div class="atualizar_usuario_pesquisar_usuario">
-          <h2>Digite o ID e o E-mail do usuário para encontra-lo,
-          Deixe em branco para cadastrar um usuário.</h2>
+          <h2>Digite o ID ou o E-mail do usuário para encontrá-lo, deixe em branco para cadastrar um usuário.</h2>
           <form action="" method="post" class="atualizar_usuario_forms">
             <div class="atualizar_usuario_pesquisar_usuario_item" id="atualizar_usuario_pesquisar_usuario_item_1">
               <label>ID Do Usuário</label>
@@ -44,26 +46,30 @@
               <label>E-mail</label>
               <input type="text" class="base_input">
             </div>
+
+            <button class="btn_blue base_botao">
+              <img src="<?=$PATH_PUBLIC?>/image/geral/botoes/v_branco_icon.svg" alt="">
+              PESQUISAR
+            </button>
           </form>
         </div>
-        
+
+        <!-- Dados Pessoais -->
         <div class="atualizar_usuario_text" id="atualizar_usuario_text_2">
           <hr class="atualizar_usuario_vertical">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/perfil_icon.svg" alt="">
           <h1 class="atualizar_usuario_text">Dados Pessoais</h1>
         </div>
-        
+
         <form action="" method="post" class="atualizar_usuario_dados_pessoais">
-          <div class="atualizar_usuario_dados_pessoais_item" id="atualizar_usuario_dados_pessoais_item_1" >
+          <div class="atualizar_usuario_dados_pessoais_item" id="atualizar_usuario_dados_pessoais_item_1">
             <label>Nome</label>
             <input type="text" class="base_input">
           </div>
-
           <div class="atualizar_usuario_dados_pessoais_item" id="atualizar_usuario_dados_pessoais_item_2">
             <label>Senha</label>
             <input type="text" class="base_input">
           </div>
-          
           <div class="atualizar_usuario_dados_pessoais_conjunto">
             <div class="atualizar_usuario_dados_pessoais_item" id="atualizar_usuario_dados_pessoais_item_3">
               <label>E-mail</label>
@@ -74,10 +80,9 @@
               <input type="text" class="base_input">
             </div>
           </div>
-
           <div class="atualizar_usuario_dados_pessoais_conjunto">
             <div class="atualizar_usuario_dados_pessoais_item" id="atualizar_usuario_dados_pessoais_item_5">
-              <label>CPf</label>
+              <label>CPF</label>
               <input type="text" class="base_input">
             </div>
             <div class="atualizar_usuario_dados_pessoais_item" id="atualizar_usuario_dados_pessoais_item_6">
@@ -87,6 +92,7 @@
           </div>
         </form>
 
+        <!-- Localização -->
         <div class="atualizar_usuario_text" id="atualizar_usuario_text_3">
           <hr class="atualizar_usuario_vertical">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/localizacao_icon.svg" alt="">
@@ -95,7 +101,6 @@
 
         <form action="" method="post" class="atualizar_usuario_localizacao">
           <div class="atualizar_usuario_localizacao_conjunto">
-            <!-- row 1 -->
             <div class="atualizar_usuario_localizacao_item" id="atualizar_usuario_localizacao_item_1">
               <label>CEP</label>
               <input type="text" class="base_input">
@@ -105,7 +110,6 @@
               <input type="text" class="base_input">
             </div>
           </div>
-          
           <div class="atualizar_usuario_localizacao_conjunto">
             <div class="atualizar_usuario_localizacao_item" id="atualizar_usuario_localizacao_item_3">
               <label>Cidade</label>
@@ -116,7 +120,6 @@
               <input type="text" class="base_input">
             </div>
           </div>
-
           <div class="atualizar_usuario_localizacao_conjunto">
             <div class="atualizar_usuario_localizacao_item" id="atualizar_usuario_localizacao_item_5">
               <label>Endereço</label>
@@ -129,11 +132,13 @@
           </div>
         </form>
 
+        <!-- Botão Mobile -->
         <div class="atualizar_usuario_text_titulo" id="atualizar_usuario_text_titulo_mobile">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/lista_caneta_icon.svg" alt="">
           <h1>ALTERAR DADOS</h1>
-        </div> 
+        </div>
 
+        <!-- Permissões -->
         <div class="atualizar_usuario_text" id="atualizar_usuario_text_4">
           <hr class="atualizar_usuario_vertical">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/engrenagem_icon.svg" alt="">
@@ -141,69 +146,65 @@
         </div>
 
         <form action="" method="post" class="atualizar_usuario_forms_permissoes">
-            <div class="atualizar_usuario_permissoes_conjunto">
-              <div class="atualizar_usuario_forms_item_permissoes">
-                <div class="toggle_container">
-                  <label class="toggle">
-                    <input type="checkbox" id="atualizar_usuario_gerenciar_carrossel">
-                    <span class="toggle_slider"></span>
-                  </label>
-                </div>
-                <div class="label_container">
-                  <label for="atualizar_usuario_gerenciar_carrossel">Desativar Usuário</label>
-                </div>
+          <div class="atualizar_usuario_permissoes_conjunto">
+            <div class="atualizar_usuario_forms_item_permissoes">
+              <div class="toggle_container">
+                <label class="toggle">
+                  <input type="checkbox" id="atualizar_usuario_gerenciar_carrossel">
+                  <span class="toggle_slider"></span>
+                </label>
               </div>
-
-              <div class="atualizar_usuario_forms_item_permissoes">
-                <div class="toggle_container">
-                  <label class="toggle">
-                    <input type="checkbox" id="atualizar_usuario_gerenciar_usuarios">
-                    <span class="toggle_slider"></span>
-                  </label>
-                </div>
-                <div class="label_container">
-                  <label for="atualizar_usuario_gerenciar_usuarios">Cliente</label>
-                </div>
+              <div class="label_container">
+                <label for="atualizar_usuario_gerenciar_carrossel">Desativar Usuário</label>
               </div>
             </div>
-            
-            <div class="atualizar_usuario_permissoes_conjunto">
-              <div class="atualizar_usuario_forms_item_permissoes">
-                <div class="toggle_container">
-                  <label class="toggle">
-                    <input type="checkbox" id="atualizar_usuario_gerenciar_produtos">
-                    <span class="toggle_slider"></span>
-                  </label>
-                </div>
-                <div class="label_container">
-                  <label for="atualizar_usuario_gerenciar_produtos">Vendedor</label>
-                </div>
+            <div class="atualizar_usuario_forms_item_permissoes">
+              <div class="toggle_container">
+                <label class="toggle">
+                  <input type="checkbox" id="atualizar_usuario_gerenciar_usuarios">
+                  <span class="toggle_slider"></span>
+                </label>
               </div>
-
-              <div class="atualizar_usuario_forms_item_permissoes">
-                <div class="toggle_container">
-                  <label class="toggle">
-                    <input type="checkbox" id="atualizar_usuario_historico_acessos">
-                    <span class="toggle_slider"></span>
-                  </label>
-                </div>
-                <div class="label_container">
-                  <label for="atualizar_usuario_historico_acessos">Administrador</label>
-                </div>
-              </div>  
+              <div class="label_container">
+                <label for="atualizar_usuario_gerenciar_usuarios">Cliente</label>
+              </div>
             </div>
           </div>
+
+          <div class="atualizar_usuario_permissoes_conjunto">
+            <div class="atualizar_usuario_forms_item_permissoes">
+              <div class="toggle_container">
+                <label class="toggle">
+                  <input type="checkbox" id="atualizar_usuario_gerenciar_produtos">
+                  <span class="toggle_slider"></span>
+                </label>
+              </div>
+              <div class="label_container">
+                <label for="atualizar_usuario_gerenciar_produtos">Vendedor</label>
+              </div>
+            </div>
+            <div class="atualizar_usuario_forms_item_permissoes">
+              <div class="toggle_container">
+                <label class="toggle">
+                  <input type="checkbox" id="atualizar_usuario_historico_acessos">
+                  <span class="toggle_slider"></span>
+                </label>
+              </div>
+              <div class="label_container">
+                <label for="atualizar_usuario_historico_acessos">Administrador</label>
+              </div>
+            </div>
+          </div>
+
+          <div class="atualizar_usuario_permissoes_conjunto">
+            <button class="btn_blue base_botao">
+              <img src="<?=$PATH_PUBLIC?>/image/geral/botoes/v_branco_icon.svg" alt="">
+              CONFIRMAR
+            </button>
+          </div>
         </form>
-        <div class="atualizar_usuario_botao_salvar">
-          <button class="atualizar_usuario_salvar base_botao">
-            <img src="<?=$PATH_PUBLIC?>/image/geral/botoes/v_branco_icon.svg" alt="">
-            <label>CONFIRMAR</label>
-          </button>
-        </div>
       </div>
-    </div> 
     </div>
   </main>
-
 </body>
 </html>
