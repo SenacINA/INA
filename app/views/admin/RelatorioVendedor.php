d59efe5f689eef89a85a9c0db1fc1f660651e0b2
Luis Fernando <luisvirissimo789@gmail.com>
2025-05-27 10:31:35 -0300

    ZA HANDO

--- app/views/admin/RelatorioVendedor.php
+++ app/views/admin/RelatorioVendedor.php
@@ -28,102 +28,18 @@
         <div class="relatorio_vendedor_text" id="relatorio_vendedor_text_1"> 
           <hr class="relatorio_vendedor_vertical">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image\geral\icons\perfil_icon.svg" alt=""/>
-          <h1 class="relatorio_vendedor_text">Dados Pessoais</h1>
+          <h1 class="relatorio_vendedor_text">Procurar o vendedor</h1>
         </div>
 
         <div class="relatorio_vendedor_pesquisar_usuario">
-          <h2>Digite o ID e o E-mail do usuário para encontra-lo,
-          Deixe em branco para cadastrar um usuário.</h2>
+          <h2>Digite o nome do usuário para encontra-lo, Após isso clique em procurar.</h2>
           <form action="" method="post" class="relatorio_vendedor_forms">
             <div class="relatorio_vendedor_pesquisar_usuario_item" id="relatorio_vendedor_pesquisar_usuario_item_1">
               <label>Nome</label>
               <input type="text" class="base_input">
             </div>
-            <div class="relatorio_vendedor_dados_pessoais_conjunto">
-              <div class="relatorio_vendedor_pesquisar_usuario_item" id="relatorio_vendedor_pesquisar_usuario_item_2">
-                <label>E-mail</label>
-                <input type="text" class="base_input">
-              </div>
-              <div class="relatorio_vendedor_pesquisar_usuario_item" id="relatorio_vendedor_pesquisar_usuario_item_2">
-                <label>Telefone</label>
-                <input type="text" class="base_input">
-              </div>
-            </div>
           </form>
         </div>
-        
-        <div class="relatorio_vendedor_text" id="relatorio_vendedor_text_2">
-          <hr class="relatorio_vendedor_vertical">
-          <img class="base_icon" src="<?=$PATH_PUBLIC?>/image\geral\icons\loja_icon.svg" alt=""/>
-          <h1 class="relatorio_vendedor_text">Loja</h1>
-        </div>
-        
-        <form action="" method="post" class="relatorio_vendedor_dados_pessoais">      
-          <div class="relatorio_vendedor_dados_pessoais_conjunto">
-            <div class="relatorio_vendedor_dados_pessoais_item" id="relatorio_vendedor_dados_pessoais_item_3">
-              <label>E-mail</label>
-              <input type="text" class="base_input">
-            </div>
-            <div class="relatorio_vendedor_dados_pessoais_item" id="relatorio_vendedor_dados_pessoais_item_4">
-              <label>Telefone</label>
-              <input type="text" class="base_input">
-            </div>
-          </div>
-
-          <div class="relatorio_vendedor_dados_pessoais_conjunto">
-            <div class="relatorio_vendedor_dados_pessoais_item" id="relatorio_vendedor_dados_pessoais_item_5">
-              <label>CPF</label>
-              <input type="text" class="base_input">
-            </div>
-            <div class="relatorio_vendedor_dados_pessoais_item" id="relatorio_vendedor_dados_pessoais_item_6">
-              <label>Telefone 2</label>
-              <input type="text" class="base_input">
-            </div>
-          </div>
-        </form>
-
-        <div class="relatorio_vendedor_text" id="relatorio_vendedor_text_3">
-          <hr class="relatorio_vendedor_vertical">
-          <img class="base_icon" src="<?=$PATH_PUBLIC?>/image\geral\icons\localizacao_icon.svg" alt=""/>
-          <h1 class="relatorio_vendedor_text">Localização</h1>
-        </div>
-
-        <form action="" method="post" class="relatorio_vendedor_localizacao">
-          <div class="relatorio_vendedor_localizacao_conjunto">
-            <!-- row 1 -->
-            <div class="relatorio_vendedor_localizacao_item" id="relatorio_vendedor_localizacao_item_1">
-              <label>CEP</label>
-              <input type="text" class="base_input">
-            </div>
-            <div class="relatorio_vendedor_localizacao_item" id="relatorio_vendedor_localizacao_item_2">
-              <label>Estado</label>
-              <input type="text" class="base_input">
-            </div>
-          </div>
-          
-          <div class="relatorio_vendedor_localizacao_conjunto">
-            <div class="relatorio_vendedor_localizacao_item" id="relatorio_vendedor_localizacao_item_3">
-              <label>Cidade</label>
-              <input type="text" class="base_input">
-            </div>
-            <div class="relatorio_vendedor_localizacao_item" id="relatorio_vendedor_localizacao_item_4">
-              <label>Bairro</label>
-              <input type="text" class="base_input">
-            </div>
-          </div>
-
-          <div class="relatorio_vendedor_localizacao_conjunto">
-            <div class="relatorio_vendedor_localizacao_item" id="relatorio_vendedor_localizacao_item_5">
-              <label>Endereço</label>
-              <input type="text" class="base_input">
-            </div>
-            <div class="relatorio_vendedor_localizacao_item" id="relatorio_vendedor_localizacao_item_6">
-              <label>Número</label>
-              <input type="text" class="base_input">
-            </div>
-          </div>
-        </form>
-
         <div class="relatorio_vendedor_text" id="relatorio_vendedor_text_4">
           <hr class="relatorio_vendedor_vertical">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image\geral\icons\lista_icon.svg" alt=""/>
--- db/banco_schema.sql
+++ db/banco_schema.sql
@@ -1,13 +1,13 @@
 create database if not exists e2_database;
 use e2_database;
-
+ 
 create table if not exists cliente(
 id_cliente int auto_increment primary key,
 nome_cliente varchar(70) not null,
 senha_cliente varchar(70) not null,
 data_registro_cliente date not null,
 email_cliente varchar(70) not null unique,
-cpf_cliente varchar(15) , 
+cpf_cliente varchar(15) ,
 genero_cliente char(1),
 cep_cliente varchar(10),
 rg_cliente varchar(14),
@@ -18,7 +18,7 @@ banner_perfil_cliente varchar(200),
 tipo_conta_cliente tinyint not null,
 status_conta_cliente tinyint
 );
-
+ 
 CREATE table if not exists  redefinicao_senha (
   id INT AUTO_INCREMENT PRIMARY KEY,
   id_cliente INT NOT NULL,
@@ -27,47 +27,53 @@ CREATE table if not exists  redefinicao_senha (
   usado BOOLEAN DEFAULT FALSE,
   FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
-
-
-
+ 
+ 
+ 
 CREATE TABLE IF NOT EXISTS endereco (
     id_endereco INT AUTO_INCREMENT PRIMARY KEY,
     rua_endereco VARCHAR(100) NOT NULL,
     bairro_endereco VARCHAR(100) NOT NULL,
     numero_endereco SMALLINT,
     referencia_endereco VARCHAR(200),
-    uf_endereco VARCHAR(2) NOT NULL,     
+    uf_endereco VARCHAR(2) NOT NULL,    
     cidade_endereco VARCHAR(50) NOT NULL,  
     id_cliente INT NOT NULL UNIQUE,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
-
-
+ 
+ 
 create table if not exists categoria( -- 6
 id_categoria int auto_increment primary key,
 nome_categoria varchar(30) not null,
 endereco_imagem_categoria varchar(200),
 index_banner_categoria tinyint default 0
 );
-
+ 
 create table if not exists subcategoria( -- 8
 id_subcategoria int auto_increment primary key,
 nome_subcategoria varchar(30) not null,
 categoria_subcategoria int, -- a ser revisado
 foreign key (categoria_subcategoria) references categoria(id_categoria)
 );
-
-CREATE TABLE IF NOT EXISTS vendedor (
-    id_vendedor      INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente       INT NOT NULL UNIQUE,  
-    nome_fantasia    VARCHAR(255) NOT NULL,
-    cnpj_vendedor    VARCHAR(30) UNIQUE NOT NULL,
-    status           ENUM('Pendente', 'Aprovado', 'Reprovado') DEFAULT 'Pendente',
-    data_requisicao  DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente) ON DELETE CASCADE
-);
  
-
+CREATE TABLE if not exists vendedor (
+  id_vendedor        INT (11)      NOT NULL AUTO_INCREMENT,
+  nome_fantasia     VARCHAR(255)      NOT NULL,
+  cnpj_vendedor      VARCHAR(30)       NOT NULL,
+  requisitos_completos TINYINT(1)      NOT NULL DEFAULT 0,
+  documento_entregue TINYINT(1)        NOT NULL DEFAULT 0,
+  status           ENUM('Pendente','Aprovado','Reprovado') NOT NULL DEFAULT 'Pendente',
+  data_requisicao   DATETIME          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id_vendedor),
+  UNIQUE KEY cnpj_vendedor (cnpj_vendedor),
+  KEY idx_vendedor_status   (status),
+  KEY idx_vendedor_data     (data_requisicao)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_general_ci;
+ 
+ 
 create table if not exists produto( -- 2
 id_produto int auto_increment primary key,
 id_vendedor int not null,
@@ -88,13 +94,13 @@ foreign key (id_vendedor) references vendedor(id_vendedor),
 foreign key (categoria_produto) references categoria(id_categoria),
 foreign key (subcategoria_produto) references subcategoria(id_subcategoria)
 );
-
-
+ 
+ 
 create table if not exists metodo_pagamento( -- 13
 id_metodo_pagamento int auto_increment primary key,
 tipo_pagamento varchar(50)
 );
-
+ 
 create table if not exists compra( -- 5
 id_compra int auto_increment primary key,
 id_cliente int not null,
@@ -104,7 +110,7 @@ id_tipo_frete int,
 foreign key (id_cliente) references cliente (id_cliente),
 foreign key (id_endereço) references endereco(id_endereco)
 );
-
+ 
 create table if not exists avaliacao( -- 7
 id_avaliacao int auto_increment primary key,
 status_avaliacao boolean not null,
@@ -118,7 +124,7 @@ foreign key (id_produto) references produto(id_produto),
 foreign key (id_cliente) references cliente(id_cliente),
 foreign key (id_vendedor) references vendedor(id_vendedor)
 );
-
+ 
 create table if not exists carrossel( -- 9
 id_carrossel int auto_increment primary key,
 id_vendedor int not null, -- foreign key
@@ -127,7 +133,7 @@ proxima_cobrança_carrossel date not null,
 foi_pago_esse_mes_carrossel boolean not null,
 foreign key (id_vendedor) references vendedor(id_vendedor)
 );
-
+ 
 create table if not exists carrinho( -- 10
 id_cliente int,
 id_produto int,
@@ -135,7 +141,7 @@ quantidade_produto smallint not null,
 foreign key (id_cliente) references cliente(id_cliente),
 foreign key (id_produto) references produto(id_produto)
 );
-
+ 
 create table if not exists imagem_produto( -- 11
 id_imagem_produto int auto_increment primary key,
 id_produto int,
@@ -143,14 +149,14 @@ endereco_imagem_produto varchar(200) not null,
 index_imagem_produto int not null,
 foreign key (id_produto) references produto(id_produto)
 );
-
+ 
 create table if not exists imagem_carrossel( -- 12
 id_imagem_carrossel int auto_increment primary key,
 id_carrossel int,
 endereco_carrossel varchar(200) not null,
 foreign key (id_carrossel) references carrossel(id_carrossel)
 );
-
+ 
 create table if not exists perfil( -- 17
 id_perfil int PRIMARY KEY AUTO_INCREMENT,
 id_cliente int,
@@ -165,7 +171,7 @@ instagram_perfil varchar(50),
 x_perfil varchar(50),
 foreign key (id_cliente) references cliente(id_cliente)
 );
-
+ 
 create table if not exists historico_acesso( -- 16
 id_historico_acesso int primary key auto_increment,
 id_cliente int,
@@ -175,7 +181,7 @@ horario_historico_acesso time,
 navegador_historico_acesso varchar(200),
 foreign key (id_cliente) references cliente(id_cliente)
 );
-
+ 
 create table if not exists promocao( -- 20
 id_promocao int primary key auto_increment,
 id_produto int unique,
@@ -186,14 +192,14 @@ data_inicio_promocao date,
 data_fim_promocao date,
 hora_inicio_promocao time,
 hora_fim_promocao time,
-foreign key (id_produto) references produto(id_produto) 
+foreign key (id_produto) references produto(id_produto)
 );
-
+ 
 create table if not exists anuncio( -- 21
-index_anuncio int unique primary key, 
+index_anuncio int unique primary key,
 endereco_imagem_anuncio varchar(200) not null  
 );
-
+ 
 create table if not exists item_compra( -- 22
 id_item_compra int primary key auto_increment,
 id_compra int,
@@ -208,27 +214,27 @@ foreign key (id_compra) references compra(id_compra),
 foreign key (id_produto) references produto(id_produto),
 foreign key (id_metodo_pagamento) references metodo_pagamento(id_metodo_pagamento)
 );
-
+ 
 create table if not exists imagem_avaliacao( -- 23
 id_imagem_avaliacao int primary key auto_increment,
 endereco_imagem_avaliacao varchar(200) not null,
 id_avaliacao int,
 foreign key (id_avaliacao) references avaliacao(id_avaliacao)
 );
-
+ 
 create table if not exists tag( -- 26
 id_tag int primary key auto_increment,
 nome_tag varchar(200) not null
 );
-
+ 
 create table if not exists tag_produto ( -- 25
-id_tag_produto integer primary key auto_increment, 
+id_tag_produto integer primary key auto_increment,
 id_produto int,  
 id_tag int,
 foreign key (id_produto) references produto(id_produto),  
 foreign key (id_tag) references tag(id_tag)  
 );
-
+ 
 create table if not exists tag_vinculo( -- 27
 id_tag_vinculo int primary key auto_increment,
 id_tag int,
@@ -238,7 +244,7 @@ foreign key (id_tag) references tag(id_tag),
 foreign key (id_categoria) references categoria(id_categoria),
 foreign key (id_subcategoria) references subcategoria(id_subcategoria)
 );
-
+ 
 CREATE TABLE IF NOT EXISTS permissao_admin (
     id_permissao INT AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT NOT NULL,
@@ -248,4 +254,3 @@ CREATE TABLE IF NOT EXISTS permissao_admin (
     acessar_historico_acesso BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente) ON DELETE CASCADE
 );
-
