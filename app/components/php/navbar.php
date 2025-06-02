bf2cddb0a27667e8a277cb26f6fcc184031dc6a5
Carlos Eduardo De L. G. <105987211+cadulucas1@users.noreply.github.com>
2025-06-02 10:49:38 -0400

    asdfghjklç
    ~çlkjhgfdsa

--- app/components/php/navbar.php
+++ app/components/php/navbar.php
@@ -138,6 +138,22 @@ function generateModalContent($user)
                       <span>Redefinir Senha</span>
                   </a>
               </li>
+              <li>
+                  <a onclick="pag(\'EditarPerfil\')" class="menu-item">
+                      <svg class="icon" viewBox="0 0 24 24" width="24" height="24" fill="#3498db">
+                          <path fill="#3498db" d="m21.7 13.35l-1 1l-2.05-2.05l1-1a.55.55 0 0 1 .77 0l1.28 1.28c.21.21.21.56 0 .77M12 18.94l6.06-6.06l2.05 2.05L14.06 21H12zM12 14c-4.42 0-8 1.79-8 4v2h6v-1.89l4-4c-.66-.08-1.33-.11-2-.11m0-10a4 4 0 0 0-4 4a4 4 0 0 0 4 4a4 4 0 0 0 4-4a4 4 0 0 0-4-4"/>
+                      </svg>
+                      <span>Editar Perfil</span>
+                  </a>
+              </li>
+              <li>
+                  <a onclick="pag(\'CadastroVendedorInfo\')" class="menu-item">
+                  <svg class="icon" viewBox="0 0 24 24" width="24" height="24" fill="#3498db">
+                    <path fill="#3498db" d="m21.41 11.58l-9-9C12.04 2.21 11.53 2 11 2H4a2 2 0 0 0-2 2v7c0 .53.21 1.04.59 1.41l.41.4c.9-.54 1.94-.81 3-.81a6 6 0 0 1 6 6c0 1.06-.28 2.09-.82 3l.4.4c.37.38.89.6 1.42.6s1.04-.21 1.41-.59l7-7c.38-.37.59-.88.59-1.41s-.21-1.04-.59-1.42M5.5 7A1.5 1.5 0 0 1 4 5.5A1.5 1.5 0 0 1 5.5 4A1.5 1.5 0 0 1 7 5.5A1.5 1.5 0 0 1 5.5 7M10 19H7v3H5v-3H2v-2h3v-3h2v3h3z"/>
+                    </svg>
+                      <span>Cadastro Vendedor</span>
+                  </a>
+              </li>
           ';
       break;
 
