6e12b0208ae023f03a75bc942b596457f786ac03
Vyenvyy <alexyvne@gmail.com>
2025-07-21 07:32:04 -0400

    Finalização do Perfil Admin

--- app/views/admin/PerfilAdmin.php
+++ app/views/admin/PerfilAdmin.php
@@ -6,11 +6,8 @@
   require_once('./utils/head.php');
 ?>
 <body>
-  <!-- Até 375px -->
+  <?php include_once("$PATH_COMPONENTS/php/navbar.php"); ?>
 
-  <?php
-    include_once("$PATH_COMPONENTS/php/navbar.php");
-  ?>
   <main>
     <div class="perfil_admin_body">
       <div class="perfil_admin_titulo">
@@ -18,7 +15,6 @@
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/perfil_icon.svg" alt="">
           <h1>PERFIL ADMIN</h1>
         </div>
-        
         <hr class="perfil_admin_linha_titulo">
       </div>
 
@@ -28,19 +24,23 @@
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/lista_icon.svg" alt="">
           <h1 class="perfil_admin_text">Seu Perfil</h1>
         </div>
+
         <div class="perfil_admin_text_2">
           <hr class="perfil_admin_vertical">
           <img class="base_icon" src="<?=$PATH_PUBLIC?>/image/geral/icons/engrenagem_icon.svg" alt="">
           <h1 class="perfil_admin_text">Permissões</h1>
         </div>
-        
+
         <div class="perfil_admin_foto">
           <label for="perfil_admin_foto">
-            <img class="perfil_admin_pfp" src="<?=$PATH_PUBLIC . $user['foto_perfil'] ?? '/image/admin/perfil_admin/perfil_img.svg' ?>" alt="" id="imgPreviewFoto">
+            <img class="perfil_admin_pfp"
+              src="<?= $PATH_PUBLIC . (!empty($user['foto_perfil']) ? $user['foto_perfil'] : '/image/admin/perfil_admin/perfil_img.svg') ?>"
+              alt=""
+              id="imgPreviewFoto">
           </label>
 
           <form action="" method="post" class="form">
-            <button class="base_botao  btn_blue" type="button" onclick="document.getElementById('fileInputFoto').click();">
+            <button class="base_botao btn_blue" type="button" onclick="document.getElementById('fileInputFoto').click();">
               <img src="<?=$PATH_PUBLIC?>/image/geral/botoes/enviar_branco_icon.svg" class="base_icon" alt="">
               <input type="file" id="fileInputFoto" name="foto" style="display: none;" accept="image/*" />
               Enviar arquivo
@@ -69,70 +69,70 @@
 
         <form action="" method="post" class="perfil_admin_forms_permissoes">
           <div class="perfil_admin_forms_item_permissoes">
-              <div class="toggle_container">
-                  <label class="toggle">
-                      <input type="checkbox" id="perfil_admin_gerenciar_carrossel" class="base_input" disabled
-                          <?= $user['gerenciar_carrossel'] ? 'checked' : '' ?>>
-                      <span class="toggle_slider"></span>
-                  </label>
-              </div>
-              <div class="label_container">
-                  <label for="perfil_admin_gerenciar_carrossel">Gerenciar carrossel</label>
-              </div>
+            <div class="toggle_container">
+              <label class="toggle">
+                <input type="checkbox" id="perfil_admin_gerenciar_carrossel" class="base_input" disabled
+                  <?= $user['gerenciar_carrossel'] ? 'checked' : '' ?>>
+                <span class="toggle_slider"></span>
+              </label>
+            </div>
+            <div class="label_container">
+              <label for="perfil_admin_gerenciar_carrossel">Gerenciar carrossel</label>
+            </div>
           </div>
 
           <div class="perfil_admin_forms_item_permissoes">
-              <div class="toggle_container">
-                  <label class="toggle">
-                      <input type="checkbox" id="perfil_admin_gerenciar_usuarios" class="base_input" disabled
-                          <?= $user['gerenciar_usuarios'] ? 'checked' : '' ?>>
-                      <span class="toggle_slider"></span>
-                  </label>
-              </div>
-              <div class="label_container">
-                  <label for="perfil_admin_gerenciar_usuarios">Gerenciar usuários</label>
-              </div>
+            <div class="toggle_container">
+              <label class="toggle">
+                <input type="checkbox" id="perfil_admin_gerenciar_usuarios" class="base_input" disabled
+                  <?= $user['gerenciar_usuarios'] ? 'checked' : '' ?>>
+                <span class="toggle_slider"></span>
+              </label>
+            </div>
+            <div class="label_container">
+              <label for="perfil_admin_gerenciar_usuarios">Gerenciar usuários</label>
+            </div>
           </div>
 
           <div class="perfil_admin_forms_item_permissoes">
-              <div class="toggle_container">
-                  <label class="toggle">
-                      <input type="checkbox" id="perfil_admin_gerenciar_produtos" class="base_input" disabled
-                          <?= $user['gerenciar_produtos'] ? 'checked' : '' ?>>
-                      <span class="toggle_slider"></span>
-                  </label>
-              </div>
-              <div class="label_container">
-                  <label for="perfil_admin_gerenciar_produtos">Gerenciar produtos</label>
-              </div>
+            <div class="toggle_container">
+              <label class="toggle">
+                <input type="checkbox" id="perfil_admin_gerenciar_produtos" class="base_input" disabled
+                  <?= $user['gerenciar_produtos'] ? 'checked' : '' ?>>
+                <span class="toggle_slider"></span>
+              </label>
+            </div>
+            <div class="label_container">
+              <label for="perfil_admin_gerenciar_produtos">Gerenciar produtos</label>
+            </div>
           </div>
 
           <div class="perfil_admin_forms_item_permissoes">
-              <div class="toggle_container">
-                  <label class="toggle">
-                      <input type="checkbox" id="perfil_admin_historico_acessos" class="base_input" disabled
-                          <?= $user['acessar_historico_acesso'] ? 'checked' : '' ?>>
-                      <span class="toggle_slider"></span>
-                  </label>
-              </div>
-              <div class="label_container">
-                  <label for="perfil_admin_historico_acessos">Acessar histórico de acessos</label>
-              </div>
+            <div class="toggle_container">
+              <label class="toggle">
+                <input type="checkbox" id="perfil_admin_historico_acessos" class="base_input" disabled
+                  <?= $user['acessar_historico_acesso'] ? 'checked' : '' ?>>
+                <span class="toggle_slider"></span>
+              </label>
+            </div>
+            <div class="label_container">
+              <label for="perfil_admin_historico_acessos">Acessar histórico de acessos</label>
+            </div>
           </div>
-      </form>
-
-        
-      </div>
-
-        <div class="perfil_admin_botao_salvar">
-          <button id='salvarEdit' class="perfil_admin_salvar">
+          <div class="perfil_admin_botao_salvar">
+          <button id='salvarEdit' class="base_botao btn_blue">
             <img src="<?=$PATH_PUBLIC?>/image/geral/botoes/v_branco_icon.svg" alt="">
             <label>SALVAR</label>
           </button>
         </div>
-      </div>
-    </div>
+        </form>
+
+        
+
+      </div> <!-- perfil_admin_grid_conteudo -->
+    </div> <!-- perfil_admin_body -->
   </main>
+
   <script src="<?=$PATH_PUBLIC?>/js/cliente/PfpInput.js"></script>
   <script type="module" src="<?=$PATH_COMPONENTS?>/js/Toast.js"></script>
   <script src="<?=$PATH_PUBLIC?>/js/admin/UpdateProfile.js"></script>
